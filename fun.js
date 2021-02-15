438611351afbd6a196fb60eaf09322afe0c03de4
ArticSpider <56699337+ArticSpider@users.noreply.github.com>
2021-02-14 23:46:06 -0500

    Added a js file and implemented a button that changes img

---
+++ fun.js
@@ -0,0 +1,4 @@
+function bobBILL() {
+  document.getElementById("linker").innerHTML = "BOO";
+  document.getElementById("changeThePic").src = "https://media.newyorker.com/photos/5bd78600daf42d0501bd51e6/1:1/w_1920,h_1920,c_limit/Treisman-Scary-Stories.jpg" 
+}
